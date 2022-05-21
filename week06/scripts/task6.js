cbbaa60610ab2eed79f9be506765a3d16aae2a60
ahollingworth <amyjohollingworth@gmail.com>
2022-05-20 20:22:37 -0600

    changes

---
+++ week06/scripts/task6.js
@@ -0,0 +1,6 @@
+const weatherPic = 'images/finland_weather.jpg';
+
+document.querySelector('#image').setAttribute('src', weatherPic);
+
+
+// "http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={e8bda0393ca6a425bd461f9fb06864cd}";
